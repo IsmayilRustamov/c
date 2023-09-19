10ed070493823d14a0f6bbfb704a49032ad16e2b
Ismayil <ismayilrustemov0606@gmail.com>
2023-09-19 23:29:21 +0400

    Initial Commit

---
+++ c/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  c
+//
+//  Created by Ismayil Rustemov on 19.09.23.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
